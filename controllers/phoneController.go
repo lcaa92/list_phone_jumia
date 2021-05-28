c45928ea38dbb4a5f997dc148be3235a4b85ac68
Leandro Araújo <leandrocaaraujo@gmail.com>
2021-05-28 19:09:10 -0300

    Use Phone model and endpoint return json

--- controllers/phoneController.go
+++ controllers/phoneController.go
@@ -2,41 +2,47 @@ package phoneController
 
 import (
 	"database/sql"
-	"fmt"
+	"encoding/json"
 	"net/http"
 
+	"github.com/lcaa92/list_phone_jumia/models"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 // ListPhone return list of Phone
 func ListPhone(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("List Phone")
-
 	db, err := sql.Open("sqlite3", "./sample.db?mode=memory&_fk=true&cache=shared")
 	defer db.Close()
 	if err != nil {
 		panic(err)
 	}
 
-	rows, err := db.Query("SELECT * FROM customer;")
+	rows, err := db.Query("SELECT phone FROM customer;")
 	if err != nil {
 		panic(err)
 	}
+	defer rows.Close()
 
-	var uid int
-	var name string
-	var phone string
+	var phoneNumber string
+	var arrResult []models.PhoneResult
 
 	for rows.Next() {
-		err = rows.Scan(&uid, &name, &phone)
+		err = rows.Scan(&phoneNumber)
 		if err != nil {
 			panic(err)
 		}
-		fmt.Println(uid)
-		fmt.Println(name)
-		fmt.Println(phone)
+
+		phoneModel := models.Phone{
+			PhoneNumber: phoneNumber,
+		}
+		phoneModel.ExtractPhoneData()
+
+		arrResult = append(arrResult, phoneModel.OutputPhone)
 	}
 
-	rows.Close()
-	fmt.Fprintln(w, "List Phones!")
+	w.Header().Set("Content-Type", "application/json")
+	err = json.NewEncoder(w).Encode(arrResult)
+	if err != nil {
+		panic(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -4,5 +4,5 @@ go 1.16
 
 require (
 	github.com/gorilla/mux v1.8.0
-	github.com/mattn/go-sqlite3 v1.14.7 // indirect
+	github.com/mattn/go-sqlite3 v1.14.7
 )
