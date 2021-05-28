7b79a033b700c5d5e73b7483802585f04e8b386a
Leandro Araújo <leandrocaaraujo@gmail.com>
2021-05-28 19:45:26 -0300

    Extract data from phone

--- models/phoneModel.go
+++ models/phoneModel.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"fmt"
+	"strings"
+)
+
 type Phone struct {
 	PhoneNumber string
 	OutputPhone PhoneResult
@@ -13,8 +18,37 @@ type PhoneResult struct {
 }
 
 func (p *Phone) ExtractPhoneData() {
-	p.OutputPhone.Country = "Brasil"
-	p.OutputPhone.State = "OK"
-	p.OutputPhone.CountryCode = "55"
-	p.OutputPhone.PhoneNumber = p.PhoneNumber
+	fmt.Printf("%v\n", p.PhoneNumber)
+	country, state, country_code, phone_number := getDataFromPhone(p.PhoneNumber)
+	p.OutputPhone.Country = country
+	p.OutputPhone.State = state
+	p.OutputPhone.CountryCode = country_code
+	p.OutputPhone.PhoneNumber = phone_number
+}
+
+func getDataFromPhone(phone_number string) (country, state, country_code, phone string) {
+	state = "OK"
+	split_phone := strings.Split(phone_number, " ")
+
+	country_code = split_phone[0]
+	country_code = strings.Replace(country_code, "(", "", -1)
+	country_code = strings.Replace(country_code, ")", "", -1)
+	country_code = strings.Join([]string{"+", country_code}, "")
+
+	phone = split_phone[1]
+
+	switch country_code {
+	case "+237":
+		country = "Cameroon"
+	case "+251":
+		country = "Ethiopia"
+	case "+212":
+		country = "Morocco"
+	case "+258":
+		country = "Mozambique"
+	case "+256":
+		country = "Uganda"
+
+	}
+	return
 }
